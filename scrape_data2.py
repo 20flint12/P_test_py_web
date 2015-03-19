bc60e23b0d66bfc6aebbf74422c85c3f28385252
20flint12 <20flint12@gmail.com>
2015-03-19 10:06:11 +0200

    2 site

--- scrape_data2.py
+++ scrape_data2.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python
 
 
+import matplotlib as plt
+
+
 import requests
 from bs4 import BeautifulSoup
 import time
 
 while True:
 
-    time.sleep(2)
+    time.sleep(5)
 
     r = requests.get("http://www.timeanddate.com/moon/usa/atlanta")
     # print r.content
@@ -24,7 +27,7 @@ while True:
         for tag in ptags:
             print tag.text
 
-    print "\n"
+    print "\n"*10
 
 
 
@@ -37,13 +40,33 @@ while True:
     # print soup.prettify()
 
     g_data = soup.find_all("td", {"valign": "top"})
-    print g_data
+    # # print g_data[0]
+    # # print "\n"*10
+    # print g_data[1]
+    # print "\n"*10
+    # print g_data[2]
+    # print "\n"*10
 
-    # for pTags in g_data:
+
+    tableTags = g_data[1].find_all("table")
+
+    for table in tableTags:
+
+        trTags = table.find_all("tr")
+        # print trTags
+
+        for tag in trTags:
+            print tag.text
+
+
+    print "\n"*10
+
+
+    # for pTags in g_data[1]:
     #     ptags = pTags.find_all("p")
-    #     # print ptags
-    #
-    #     for tag in ptags:
+    #     print ptags
+
+        # for tag in ptags:
     #         print tag.text
     #
     # print "\n"
