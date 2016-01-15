aee389f366193fe529dc6de4d20c3e021e793f4a
Sergey <20flint12@gmail.com>
2016-01-15 14:19:15 +0200

    cv

--- cv_test.py
+++ cv_test.py
@@ -1,9 +1,9 @@
 __author__ = 'sergey'
 
 
-# import numpy as np
-# import cv2
-#
+import numpy as np
+import cv2
+
 # # Create a black image
 # img = np.zeros((512,512,3), np.uint8)
 #
@@ -22,42 +22,133 @@ __author__ = 'sergey'
 # cv2.destroyAllWindows()
 
 
+# ********************************
+#
+# import cv2
+# import numpy as np
+#
+# def nothing(x):
+#     pass
+#
+# # Create a black image, a window
+# img = np.zeros((300,512,3), np.uint8)
+# cv2.namedWindow('image')
+#
+# # create trackbars for color change
+# cv2.createTrackbar('R','image',0,255,nothing)
+# cv2.createTrackbar('G','image',0,255,nothing)
+# cv2.createTrackbar('B','image',0,255,nothing)
+#
+# # create switch for ON/OFF functionality
+# switch = '0 : OFF \n1 : ON'
+# cv2.createTrackbar(switch, 'image',0,1,nothing)
+#
+# while(1):
+#     cv2.imshow('image',img)
+#     k = cv2.waitKey(1) & 0xFF
+#     if k == 27:
+#         break
+#
+#     # get current positions of four trackbars
+#     r = cv2.getTrackbarPos('R','image')
+#     g = cv2.getTrackbarPos('G','image')
+#     b = cv2.getTrackbarPos('B','image')
+#     s = cv2.getTrackbarPos(switch,'image')
+#
+#     if s == 0:
+#         img[:] = 0
+#     else:
+#         img[:] = [b,g,r]
+#
+# cv2.destroyAllWindows()
+#
 
 
-import cv2
-import numpy as np
+# ***********************************
 
-def nothing(x):
-    pass
 
-# Create a black image, a window
-img = np.zeros((300,512,3), np.uint8)
-cv2.namedWindow('image')
+# cap = cv2.VideoCapture(0)
+#
+# while(True):
+#     # Capture frame-by-frame
+#     ret, frame = cap.read()
+#
+#     # Our operations on the frame come here
+#     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+#
+#     # Display the resulting frame
+#     cv2.imshow('frame',gray)
+#     if cv2.waitKey(1) & 0xFF == ord('q'):
+#         break
+#
+# # When everything done, release the capture
+# cap.release()
+# cv2.destroyAllWindows()
 
-# create trackbars for color change
-cv2.createTrackbar('R','image',0,255,nothing)
-cv2.createTrackbar('G','image',0,255,nothing)
-cv2.createTrackbar('B','image',0,255,nothing)
 
-# create switch for ON/OFF functionality
-switch = '0 : OFF \n1 : ON'
-cv2.createTrackbar(switch, 'image',0,1,nothing)
+# ***********************************
 
-while(1):
-    cv2.imshow('image',img)
-    k = cv2.waitKey(1) & 0xFF
-    if k == 27:
+# Playing Video from file
+
+# cap = cv2.VideoCapture('vtest.avi')
+#
+# while(cap.isOpened()):
+#     ret, frame = cap.read()
+#
+#     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+#
+#     cv2.imshow('frame',gray)
+#     if cv2.waitKey(1) & 0xFF == ord('q'):
+#         break
+#
+# cap.release()
+# cv2.destroyAllWindows()
+
+
+
+
+# ***********************************
+
+# Saving a Video
+
+cap = cv2.VideoCapture(0)
+
+# Define the codec and create VideoWriter object
+# fourcc = cv2.VideoWriter_fourcc('M','J','P','G')
+fourcc = cv2.cv.CV_FOURCC(*'XVID')
+out = cv2.VideoWriter('output.avi',fourcc, 20.0, (640,480))
+
+while cap.isOpened():
+    ret, frame = cap.read()
+    if ret==True:
+        frame = cv2.flip(frame,0)
+
+        if cv2.waitKey(1) & 0xFF == ord('n'):
+            # write the flipped frame
+            out.write(frame)
+
+        cv2.imshow('frame',frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    else:
         break
 
-    # get current positions of four trackbars
-    r = cv2.getTrackbarPos('R','image')
-    g = cv2.getTrackbarPos('G','image')
-    b = cv2.getTrackbarPos('B','image')
-    s = cv2.getTrackbarPos(switch,'image')
-
-    if s == 0:
-        img[:] = 0
-    else:
-        img[:] = [b,g,r]
-
+# Release everything if job is finished
+cap.release()
+out.release()
 cv2.destroyAllWindows()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
