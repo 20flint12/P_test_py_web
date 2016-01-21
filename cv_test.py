713cc4dd460d91640acbb125d00346dd0d7ab18c
20flint12 <20flint12@gmail.com>
2016-01-21 10:20:14 +0200

    first from H+

--- cv_test.py
+++ cv_test.py
@@ -9,13 +9,10 @@ __author__ = 'sergey'
 # # sys.path.append('/usr/local/lib/python2.7/site-packages')
 # sys.path.append('/usr/lib/python2.7/dist-packages')
 
-<<<<<<< HEAD
 # import sys
 
 # sys.path.append('/usr/local/lib/python2.7/site-packages')
 # sys.path.append('/usr/lib/python2.7/dist-packages')
-=======
->>>>>>> c3b017153dbafd1d0433ce99ad14bcaaf6e112e7
 
 # import numpy as np
 import cv2
@@ -165,51 +162,48 @@ import cv2
 
 # http://opencv-python-tutroals.readthedocs.org/en/latest/py_tutorials/py_ml/py_knn/py_knn_opencv/py_knn_opencv.html
 
-from matplotlib import pyplot as plt
+# from matplotlib import pyplot as plt
+#
+# img = cv2.imread('digit.png')
+# gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+# cv2.imshow('frame', gray)
+# cv2.waitKey(0)
+#
+#
+#
+# # Now we split the image to 5000 cells, each 20x20 size
+# cells = [np.hsplit(row,100) for row in np.vsplit(gray,50)]
+#
+# # Make it into a Numpy array. It size will be (50,100,20,20)
+# x = np.array(cells)
+#
+# # Now we prepare train_data and test_data.
+# train = x[:,:50].reshape(-1,400).astype(np.float32) # Size = (2500,400)
+# test = x[:,50:100].reshape(-1,400).astype(np.float32) # Size = (2500,400)
+#
+# # Create labels for train and test data
+# k = np.arange(10)
+# train_labels = np.repeat(k,250)[:,np.newaxis]
+# test_labels = train_labels.copy()
+#
+# # Initiate kNN, train the data, then test it with test data for k=1
+# knn = cv2.KNearest()
+# knn.train(train,train_labels)
+# ret,result,neighbours,dist = knn.find_nearest(test,k=5)
+#
+# # Now we check the accuracy of classification
+# # For that, compare the result with test_labels and check which are wrong
+# matches = result==test_labels
+# correct = np.count_nonzero(matches)
+# accuracy = correct*100.0/result.size
+# print accuracy
+#
+# # Release everything if job is finished
+# cap.release()
+# out.release()
+# cv2.destroyAllWindows()
 
-img = cv2.imread('digit.png')
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-cv2.imshow('frame', gray)
-cv2.waitKey(0)
 
-
-
-# Now we split the image to 5000 cells, each 20x20 size
-cells = [np.hsplit(row,100) for row in np.vsplit(gray,50)]
-
-# Make it into a Numpy array. It size will be (50,100,20,20)
-x = np.array(cells)
-
-# Now we prepare train_data and test_data.
-train = x[:,:50].reshape(-1,400).astype(np.float32) # Size = (2500,400)
-test = x[:,50:100].reshape(-1,400).astype(np.float32) # Size = (2500,400)
-
-# Create labels for train and test data
-k = np.arange(10)
-train_labels = np.repeat(k,250)[:,np.newaxis]
-test_labels = train_labels.copy()
-
-# Initiate kNN, train the data, then test it with test data for k=1
-knn = cv2.KNearest()
-knn.train(train,train_labels)
-ret,result,neighbours,dist = knn.find_nearest(test,k=5)
-
-# Now we check the accuracy of classification
-# For that, compare the result with test_labels and check which are wrong
-matches = result==test_labels
-correct = np.count_nonzero(matches)
-accuracy = correct*100.0/result.size
-print accuracy
-
-<<<<<<< HEAD
-# Release everything if job is finished
-cap.release()
-out.release()
-cv2.destroyAllWindows()
-
-
-=======
->>>>>>> c3b017153dbafd1d0433ce99ad14bcaaf6e112e7
 
 
 
